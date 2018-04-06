6f9215c20a08dbfe22a0afd6f3181fe513db0b5d
Damian Szymanski <damian@assembla.com>
2018-04-06 15:30:44 +0200

    Update key url

--- recipes/repository.rb
+++ recipes/repository.rb
@@ -30,7 +30,7 @@ when 'debian'
     uri "#{repository_base}apt/"
     distribution node['lsb']['codename']
     components ['main']
-    key "https://download.gluster.org/pub/gluster/glusterfs/#{node['gluster']['version']}/LATEST/rsa.pub"
+    key "https://download.gluster.org/pub/gluster/glusterfs/#{node['gluster']['version']}/rsa.pub"
     deb_src false
     not_if do
       File.exist?("/etc/apt/sources.list.d/glusterfs-#{node['gluster']['version']}.list")
